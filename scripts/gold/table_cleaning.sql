d05dea35fea6494eb3fb53b579f851d1ce8e4f80
rlfelisilda <rlf@gmail.com>
2025-11-01 08:01:33 +0800

    update the cleaning table script to double check the data integration for the genders from crm and erp source system

--- scripts/gold/table_cleaning.sql
+++ scripts/gold/table_cleaning.sql
@@ -1,20 +1,13 @@
-SELECT DISTINCT
-	ci.cst_gndr,
-	ca.gen
-FROM silver.crm_cust_info AS ci
-LEFT JOIN silver.erp_cust_az12 AS ca
-	   ON ci.cst_key = ca.cid
-LEFT JOIN silver.erp_loc_a101 AS la
-	   ON ci.cst_key = la.cid
-ORDER BY
-	1 ASC, 2 ASC; 
+-- Data Integration
 
-
+-- Asked the Source System EXPERT which is the Master Table for these values.
+-- NULLs often come from JOINED TABLES
+-- NULL will appear if SQL finds no match.
 SELECT DISTINCT
 	ci.cst_gndr,
 	ca.gen,
 	CASE
-		WHEN	ci.cst_gndr != 'n/a'	THEN ci.cst_gndr
+		WHEN	ci.cst_gndr != 'n/a'	THEN	ci.cst_gndr
 		ELSE	COALESCE(ca.gen, 'n/a')
 	END AS new_gen
 FROM silver.crm_cust_info AS ci
@@ -24,3 +17,8 @@ LEFT JOIN silver.erp_loc_a101 AS la
 	   ON ci.cst_key = la.cid
 ORDER BY
 	1, 2;
+
+-- Data Integration, integrating two different source system in One. 
+-- This is exactly what we call data integration
+-- Why we tried to get data from different source system in order to get rich
+-- information in the data warehouse.
